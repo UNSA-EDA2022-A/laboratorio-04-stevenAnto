f8a3d2e9d949ec046a09ad592ab6922eb90471ef
stevenAnto <ecalcinap@unsa.edu.pe>
2022-06-05 08:44:52 -0500

    se implemento remove, insert

--- src/main/java/com/example/project/SinglyLinkedList.java
+++ src/main/java/com/example/project/SinglyLinkedList.java
@@ -1,171 +1,196 @@
-package com.example.project;
+//package com.example.project;
 
 public class SinglyLinkedList<T> {
-    private Node<T> first; // Primero nodo de la lista
-    private int size; // Tamano de la lista
+  private Node<T> first; // Primero nodo de la lista
+  private int size; // Tamano de la lista
 
-    // Constructor (crea lista vacia)
-    SinglyLinkedList() {
-        first = null;
-        size = 0;
+  // Constructor (crea lista vacia)
+  SinglyLinkedList() {
+    first = null;
+    size = 0;
+  }
+
+  // Retorna el tamano de la lista
+  public int size() {
+    return size;
+  }
+
+  // Devuelve true si la lista esta vazia o false caso contrario
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+
+  // Adiciona v al inicio de la lista
+  public void addFirst(T v) {
+    Node<T> newNode = new Node<T>(v, first);
+    first = newNode;
+    size++;
+  }
+
+  // Adiciona v al final de la lista
+  public void addLast(T v) {
+    Node<T> newNode = new Node<T>(v, null);
+    if (isEmpty()) {
+      first = newNode;
+    } else {
+      Node<T> cur = first;
+      while (cur.getNext() != null)
+	cur = cur.getNext();
+      cur.setNext(newNode);
+    }
+    size++;
+  }
+
+  // Retorna el primer valor de la lista (o null si la lista esta vacia)
+  public T getFirst() {
+    if (isEmpty())
+      return null;
+    return first.getValue();
+  }
+
+  // Retorna el ultimo valor de la lista (o null si la lista esta vazia)
+  public T getLast() {
+    if (isEmpty())
+      return null;
+    Node<T> cur = first;
+    while (cur.getNext() != null)
+      cur = cur.getNext();
+    return cur.getValue();
+  }
+
+  // Elimina el primer elemento de la lista (si esta vacia no hara nada)
+  public void removeFirst() {
+    if (isEmpty())
+      return;
+    first = first.getNext();
+    size--;
+  }
+
+  // Elimina el ultimo elemento de la lista (si esta vacia no hara nada)
+  public void removeLast() {
+    if (isEmpty())
+      return;
+    if (size == 1) {
+      first = null;
+    } else {
+      // Ciclo con for y uso de size para mostrar alternativa al while
+      Node<T> cur = first;
+      for (int i = 0; i < size - 2; i++)
+	cur = cur.getNext();
+      cur.setNext(cur.getNext().getNext());
+    }
+    size--;
+  }
+
+  // Convierte la lista para um String
+  public String toString() {
+    String str = "{";
+    Node<T> cur = first;
+    while (cur != null) {
+      str += cur.getValue();
+      cur = cur.getNext();
+      if (cur != null)
+	str += ",";
+    }
+    str += "}";
+    return str;
+  }
+
+  // NUEVOS METODOS
+
+  // Elimina aquellos nodos de la lista que esten duplicados
+  public void deleteDuplicates() {
+
+  }
+
+  // Inserta un nuevo nodo en una posicion especifica de la lista
+  public void insertNth(T data, int position) {
+    System.out.println("se introduc nuevo nodo en posicion : "+position);
+    Node <T> cur = searchIndex(position-1);
+    if (position == this.size) addLast(data);
+    if ( cur == null) addFirst(data);
+    if ( cur != null){
+      cur.setNext( new Node<T>(data, cur.getNext()));
     }
 
-    // Retorna el tamano de la lista
-    public int size() {
-        return size;
+  }
+
+  // Elimina el nodo de una posicion especifica de la lista
+  public void deleteNth(int position) {
+    System.out.println("se eliminara a : "+ position);
+    Node <T> cur = searchIndex(position-1);
+    if (cur != null) {
+    cur.setNext(cur.getNext().getNext());
+    size--;
+    }else System.out.println("indice no encontrado o vacio");
+
+  }
+  public Node <T> searchIndex(int i){
+    System.out.println("se buscara indice"+ i);
+    Node <T> cur = this.first;
+    int contador = 0;
+    while (i >= 0 && cur != null && contador != i ){
+      cur = cur.getNext();
+      contador++;
     }
+    if (cur != null) return cur;
+    return null;
 
-    // Devuelve true si la lista esta vazia o false caso contrario
-    public boolean isEmpty() {
-        return (size == 0);
-    }
+  }
 
-    // Adiciona v al inicio de la lista
-    public void addFirst(T v) {
-        Node<T> newNode = new Node<T>(v, first);
-        first = newNode;
-        size++;
-    }
+  public static void main(final String[] args) {
 
-    // Adiciona v al final de la lista
-    public void addLast(T v) {
-        Node<T> newNode = new Node<T>(v, null);
-        if (isEmpty()) {
-            first = newNode;
-        } else {
-            Node<T> cur = first;
-            while (cur.getNext() != null)
-                cur = cur.getNext();
-            cur.setNext(newNode);
-        }
-        size++;
-    }
+    // testExercicio1();
+     testExercicio2();
+    testExercicio3();       
 
-    // Retorna el primer valor de la lista (o null si la lista esta vacia)
-    public T getFirst() {
-        if (isEmpty())
-            return null;
-        return first.getValue();
-    }
+  }
 
-    // Retorna el ultimo valor de la lista (o null si la lista esta vazia)
-    public T getLast() {
-        if (isEmpty())
-            return null;
-        Node<T> cur = first;
-        while (cur.getNext() != null)
-            cur = cur.getNext();
-        return cur.getValue();
-    }
+  public static void testExercicio1(){
 
-    // Elimina el primer elemento de la lista (si esta vacia no hara nada)
-    public void removeFirst() {
-        if (isEmpty())
-            return;
-        first = first.getNext();
-        size--;
-    }
+    SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
 
-    // Elimina el ultimo elemento de la lista (si esta vacia no hara nada)
-    public void removeLast() {
-        if (isEmpty())
-            return;
-        if (size == 1) {
-            first = null;
-        } else {
-            // Ciclo con for y uso de size para mostrar alternativa al while
-            Node<T> cur = first;
-            for (int i = 0; i < size - 2; i++)
-                cur = cur.getNext();
-            cur.setNext(cur.getNext().getNext());
-        }
-        size--;
-    }
+    list.addLast(47);
+    list.addLast(89);
+    list.addLast(56);
+    list.addLast(89);
+    list.addLast(56);
 
-    // Convierte la lista para um String
-    public String toString() {
-        String str = "{";
-        Node<T> cur = first;
-        while (cur != null) {
-            str += cur.getValue();
-            cur = cur.getNext();
-            if (cur != null)
-                str += ",";
-        }
-        str += "}";
-        return str;
-    }
+    System.out.println(list);
 
-    // NUEVOS METODOS
+    list.deleteDuplicates();
 
-    // Elimina aquellos nodos de la lista que esten duplicados
-    public void deleteDuplicates() {
+    System.out.println(list);
+  }
 
-    }
+  public static void testExercicio2(){
 
-    // Inserta un nuevo nodo en una posicion especifica de la lista
-    public void insertNth(T data, int position) {
+    SinglyLinkedList<Character> list = new SinglyLinkedList<Character>();
 
-    }
+    list.addLast('a');
+    list.addLast('b');
+    list.addLast('d');
 
-    // Elimina el nodo de una posicion especifica de la lista
-    public void deleteNth(int position) {
+    System.out.println(list);
 
-    }
+    list.insertNth('c', 2);
 
-    public static void main(final String[] args) {
+    System.out.println(list);
+  }
 
-        // testExercicio1();
-        // testExercicio2();
-        testExercicio3();       
+  public static void testExercicio3(){
 
-    }
+    SinglyLinkedList<Character> list = new SinglyLinkedList<Character>();
 
-    public static void testExercicio1(){
+    list.addLast('a');
+    list.addLast('b');
+    list.addLast('d');
 
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+    System.out.println(list);
 
-        list.addLast(47);
-        list.addLast(89);
-        list.addLast(56);
-        list.addLast(89);
-        list.addLast(56);
+    list.deleteNth(2);
 
-        System.out.println(list);
-
-        list.deleteDuplicates();
-
-        System.out.println(list);
-    }
-
-    public static void testExercicio2(){
-
-        SinglyLinkedList<Character> list = new SinglyLinkedList<Character>();
-
-        list.addLast('a');
-        list.addLast('b');
-        list.addLast('d');
-
-        System.out.println(list);
-
-        list.insertNth('c', 2);
-
-        System.out.println(list);
-    }
-
-    public static void testExercicio3(){
-
-        SinglyLinkedList<Character> list = new SinglyLinkedList<Character>();
-
-        list.addLast('a');
-        list.addLast('b');
-        list.addLast('d');
-
-        System.out.println(list);
-
-        list.deleteNth(2);
-
-        System.out.println(list);
-    }
+    System.out.println(list);
+  }
 
 }
